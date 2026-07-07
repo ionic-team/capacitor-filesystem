b17886c16828c9416795a6e10914a494603e10c5
jcesarmobile <jcesarmobile@gmail.com>
2026-07-07 16:20:23 +0200

    chore!: Align plugin baselines with core

--- Package.swift
+++ Package.swift
@@ -3,14 +3,14 @@ import PackageDescription
 
 let package = Package(
     name: "CapacitorFilesystem",
-    platforms: [.iOS(.v15)],
+    platforms: [.iOS(.v16)],
     products: [
         .library(
             name: "CapacitorFilesystem",
             targets: ["FilesystemPlugin"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "8.0.0"),
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "9.0.0-alpha.5"),
         .package(url: "https://github.com/ionic-team/ion-ios-filesystem.git", from: "1.1.1")
     ],
     targets: [
