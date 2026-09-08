51a4ac1851f58f4bce17ab8454e0e53131207a9e
OS-pedrogustavobilro <pedro.gustavo.bilro@outsystems.com>
2026-09-08 17:36:19 +0100

    chore(ios): update to offical version on SPM

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "8.0.0"),
-        .package(url: "https://github.com/ionic-team/ion-ios-filesystem.git", branch: "fix/IONIC-102/canonical-paths")
+        .package(url: "https://github.com/ionic-team/ion-ios-filesystem.git", from: "1.0.4")
     ],
     targets: [
         .target(
